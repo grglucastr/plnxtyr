7091028d5657a03c3aacfffcb43c7b1fc4961c38
George Lucas T. Bentes <george.bentes@gmail.com>
2016-12-27 14:10:02 -0400

    Added some HTTP methods to handle requests.

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var userController = require('../controllers/user-controller');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+var users = function(){
+    router.route('/')
+        .post(userController.postNewUser);
 
-module.exports = router;
+    router.route('/:id')
+        .get(userController.getUser);
+
+    return router;
+};
+
+module.exports = users();
