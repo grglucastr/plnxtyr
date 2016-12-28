4583fa9561fb2083a2517564e43defc129b3bf09
George Bentes <george.bentes@gmail.com>
2016-12-28 11:02:15 -0400

    New tests with services and controllers

--- tests/user-tests.js
+++ tests/user-tests.js
@@ -1,7 +1,61 @@
 var should = require('should');
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/plnxtyr');
+var userService = require('../services/user-service')(db);
+var userController = require('../controllers/user-controller');
 
 describe('User test controller', function(){
-    it('should pass', function(done){
-        done();
+    it('Should be ok. Email exists!', function(done){
+        var obj = {};
+        obj.email = 'george.bentes@gmail.com';
+        userController.validateEmail(obj, function(err, emailExists){
+            if(err){
+                return done(err);
+            }
+            emailExists.should.be.ok();
+            done();
+        });
+    });
+
+    it('Should be not ok. Email not exists!', function(done){
+        var obj = {};
+        obj.email = 'georgebentes@gmail.com';
+        userController.validateEmail(obj, function(err, emailExists){
+            if(err){
+                return done(err);
+            }
+            emailExists.should.be.false();
+            done();
+        });
+    });
+});
+
+describe('User service tests', function(){
+    it('Should get a user by its email', function(done){
+        var email = 'george.bentes@gmail.com';
+        userService.getUserByParam(email, function(err, user){
+
+            if(err){
+                return done(err);
+            }
+
+            should.exist(user);
+            done();
+        });
+
+    });
+
+    it('Should get a user by its id', function(done){
+        var id = '5863b2fc1d41dd4f7c5fde04';
+        userService.getUserByParam(id, function(err, user){
+
+            if(err){
+                return done(err);
+            }
+
+            should.exist(user);
+            done();
+        });
     });
 });
