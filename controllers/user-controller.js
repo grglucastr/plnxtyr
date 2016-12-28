5d3466b60f1afdc2863889029ed0b259659367ec
George Bentes <george.bentes@gmail.com>
2016-12-28 11:45:01 -0400

    Cleaning code

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,24 +1,19 @@
-var mongo = require('mongodb');
-var monk = require('monk');
-var db = monk('localhost:27017/plnxtyr');
-
+var db = require('../util/database');
 var userService = require('../services/user-service')(db);
+var functions = require('../util/functions');
 
 var userController = function(){
 
     var getUser = function(req, res){
         userService.getUserByParam(req.params.id, function(err, user){
-            if(err){
-                return res.status(500).json(err);
-            }
-            return res.status(200).json(user);
+            return functions.printResponse(res, user, 200, err);
         });
     };
 
     var postNewUser = function(req, res){
         // Password encryption
         if(req.body.password){
-            req.body.password = handleStringEncryption(req.body.password);
+            req.body.password = functions.handleStringEncryption(req.body.password);
         }
 
         validateEmail(req.body, function(errValidate, emailExists){
@@ -31,10 +26,7 @@ var userController = function(){
             }
 
             userService.saveNewUser(req.body, function(err, user){
-                if(err){
-                    return res.status(500).json(err);
-                }
-                return res.status(201).json(user);
+                return functions.printResponse(res, user, 201, err);
             });
         });
     };
@@ -54,22 +46,6 @@ var userController = function(){
             }
             return callback(null, userExists);
         });
-    };
-
-    function handleStringEncryption(str){
-        const crypto = require('crypto');
-        const secretkey = "Twilight 0f The Thunder G()d!";
-        const cipher = crypto.createCipher('aes192', secretkey);
-        var encrypted =  cipher.update(str, 'utf8', 'hex');
-        return encrypted += cipher.final('hex');
-    };
-
-    function handleStringDecryption(str){
-        const crypto = require('crypto');
-        const secretkey = "Twilight 0f The Thunder G()d!";
-        const decipher = crypto.createDecipher('aes192', secretkey);
-        var decrypted = decipher.update(str, 'hex', 'utf8');
-        return decrypted += decipher.final('utf8');
     };
 
     return{
